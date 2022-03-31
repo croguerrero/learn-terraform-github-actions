3eea8c601e19c703232d34ae6d7d491b8d9e418a
MARCELO GUERRERO <32203163+croguerrero@users.noreply.github.com>
2022-03-31 11:07:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,14 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = "${file("installapache.sh")}"
+   user_data = << EOF
+		      #! /bin/bash
+          sudo apt-get update
+		      curl -fsSL https://get.docker.com -o get-docker.sh
+          sh get-docker.sh
+          make shellcheck
+		      service docker start 
+	EOF
 }
 
 resource "aws_security_group" "web-sg" {
