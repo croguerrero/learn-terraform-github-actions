572d41f7d98706466696e254db0de1b534719a46
MARCELO GUERRERO <32203163+croguerrero@users.noreply.github.com>
2022-04-01 14:05:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,8 +46,8 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
